fe57b84de368a820b9d2a6186320d31dbf8b1fdc
Vihaan Rao <vihaanvrao22@gmail.com>
2023-11-22 23:19:10 -0600

    fix: compilation errors from macros in memory.h

--- chunk.c
+++ chunk.c
@@ -3,6 +3,7 @@
 
 #include "chunk.h"
 #include "memory.h"
+
 void init_chunk(Chunk *chunk) {
 	chunk->count = 0;
 	chunk->capacity = 0;
@@ -11,8 +12,8 @@ void init_chunk(Chunk *chunk) {
 
 void write_chunk(Chunk *chunk, uint8_t byte) {
 	if (chunk->capacity < chunk->capacity + 1) { //check if curr arr has capacity for new byte
-		int old_capacity = chunk->capacity;
-		chunk->capacity = GROW_CAPACTIY(old_capacity);
+		int old_capacity = chunk->capacity; chunk->capacity = GROW_CAPACITY(old_capacity);
+
 		chunk->code = GROW_ARRAY(uint8_t, chunk->code, old_capacity, chunk->capacity);
 	}
 
--- memory.h
+++ memory.h
@@ -2,11 +2,13 @@
 #define clox_memory_h
 
 #include "common.h"
+#include <stddef.h>
 
-#define GROW_CAPACITY(capacity) \ ((capacity) < 8 ? 8 : (capacity * 2)) //macro to grow capacity if < 8 bytes
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
 
-#define GROW_ARRAY(type, pointer, old_count, new_count) \ (type*)reallocate(pointer, sizeof(type) * old_count), \
-		  sizeof(type) * (new_count))
+#define GROW_ARRAY(type, pointer, old_count, new_count) \
+    (type*)reallocate(pointer, sizeof(type) * (old_count), sizeof(type) * (new_count))
 
 void* reallocate(void* pointer, size_t old_size, size_t new_size);
 #endif 
