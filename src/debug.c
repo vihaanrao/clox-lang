6482c6a25c5901c1c5419e603f3f758bc08f83e0
Vihaan Rao <vihaanvrao22@gmail.com>
2023-12-07 03:03:36 -0600

    feat(disassembler): added debug core functions

--- src/debug.c
+++ src/debug.c
@@ -1,5 +1,7 @@
+#include <stdint.h>
 #include <stdio.h>
 
+#include "chunk.h"
 #include "debug.h"
 
 void disassemble_chunk(Chunk *chunk, const char *name) {
@@ -10,3 +12,20 @@ void disassemble_chunk(Chunk *chunk, const char *name) {
   }
 }
 
+int disassemble_instruction(Chunk* chunk, int offset) {
+  printf("%04d", offset);
+
+  uint8_t instruction = chunk->code[offset];
+  switch (instruction) {
+    case OP_RETURN:
+      return simple_instruction("OP_RETURN", offset);
+    default:
+      printf("Unkown opcode %d\n", instruction);
+      return offset + 1;
+  }
+}
+
+static int simple_instruction(const char* name, int offset) {
+  printf("%s\n", name);
+  return offset + 1;
+}
