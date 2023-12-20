b159677559b4262f7dfab7fc747ff33ca8ccbeba
Vihaan Rao <vihaanvrao22@gmail.com>
2023-12-20 15:20:37 -0600

    chore: fix file directory

--- src/chunk.c
+++ src/chunk.c
@@ -3,11 +3,13 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 void init_chunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  init_value_array(&chunk->constants);
 }
 
 void write_chunk(Chunk *chunk, uint8_t byte) {
@@ -24,5 +26,12 @@ void write_chunk(Chunk *chunk, uint8_t byte) {
 
 void free_chunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  free_value_array(&chunk->constants);
   init_chunk(chunk);
 }
+
+int add_const(Chunk *chunk, Value value) {
+  write_value_array(&chunk->constants, value);
+  return chunk->constants.count - 1; //return index where const appended to locate later
+}
+
